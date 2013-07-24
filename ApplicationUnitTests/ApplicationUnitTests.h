947f2731e24292b9d075c2ccfcd49f76f414467c
Peng Wang <rocinwinter@gmail.com>
2013-07-24 11:47:41 +0800

    Reorganize the folder structure.

---
+++ ApplicationUnitTests/ApplicationUnitTests.h
@@ -0,0 +1,13 @@
+//
+//  ApplicationUnitTests.h
+//  ApplicationUnitTests
+//
+//  Created by Roc on 13-7-24.
+//  Copyright (c) 2013年 Roc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ApplicationUnitTests : SenTestCase
+
+@end
---
+++ ApplicationUnitTests/ApplicationUnitTests.m
@@ -0,0 +1,32 @@
+//
+//  ApplicationUnitTests.m
+//  ApplicationUnitTests
+//
+//  Created by Roc on 13-7-24.
+//  Copyright (c) 2013年 Roc. All rights reserved.
+//
+
+#import "ApplicationUnitTests.h"
+
+@implementation ApplicationUnitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ApplicationUnitTests");
+}
+
+@end
